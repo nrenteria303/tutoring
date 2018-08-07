b1b4e02baf557c9d3f8389f90212c145d1571f22
Nick Renteria <Nick Renteria>
2018-08-07 00:36:32 -0600

    begin menu icn functionality

---
+++ scripts/app.js
@@ -0,0 +1,8 @@
+// Dropdown Menu Showing/Hiding
+// +_+_+_+_+_+_+_+_+_+_+_+_+_+_
+
+function dropNavMenu() {
+    document.getElementById('nav-dropdown').classList.toggle('hide');
+    document.getElementById('nav-dropdown').classList.toggle('show');
+}
+
