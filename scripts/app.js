cb2b70b2e0feb68b7a34ed970789e29c3b74a3bb
Nick Renteria <Nick Renteria>
2018-08-14 21:49:58 -0600

    about page mq's and layout

--- scripts/app.js
+++ scripts/app.js
@@ -6,6 +6,20 @@ function dropNavMenu() {
     document.getElementById('nav-dropdown').classList.toggle('show');
 }
 
+function showShortVersion() {
+    document.getElementById('about-story-long').classList.remove('nav-clicked');
+    document.getElementById('about-story-short').classList.add('nav-clicked');
+    document.getElementById('my-story-wrapper').style.display = 'none';
+    document.getElementById('my-stats-wrapper').style.display = 'block';
+}
+
+function showLongVersion() {
+    document.getElementById('about-story-short').classList.remove('nav-clicked');
+    document.getElementById('about-story-long').classList.add('nav-clicked');
+    document.getElementById('my-stats-wrapper').style.display = 'none';
+    document.getElementById('my-story-wrapper').style.display = 'block';
+}
+
 window.onclick = function(e) {
     if (!e.target.matches('#nav-menu-icn')) {
         let dropdowns = document.getElementById('nav-dropdown');
